55ade84500f732f24d62becc96d50b3a7202139b
Michealchuks <michealchuks66@gmail.com>
2022-07-08 08:02:24 -0700

    my commit message

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
-    """Return a new dictionary with all values multipled by 2."""
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+    "returns a new dictionary with all values multiplied by 2"
+
+    b_dictionary = {}
+    for i in a_dictionary:
+        b_dictionary[i] = a_dictionary[i] * 2
+    return (b_dictionary)
